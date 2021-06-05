18fc35e34698e5afd0d8f4072478f3693a351a0a
John-Alex07 <jonualex07895998212@gmail.com>
2021-06-05 19:04:17 +0530

    basic_interface

--- app/src/main/java/com/jora/hambugger/MainActivity.java
+++ app/src/main/java/com/jora/hambugger/MainActivity.java
@@ -3,6 +3,8 @@ package com.jora.hambugger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buzz(View view) {
+        ImageView buzzButton = findViewById(R.id.buzzButton);
+        buzzButton.setImageResource(R.drawable.buzz_button_pressed);
+
+
     }
 }
